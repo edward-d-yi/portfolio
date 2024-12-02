9805b2628860c501ce691fb6c7546438417d5490
Edward-D-Yi <163349148+Edward-D-Yi@users.noreply.github.com>
2024-12-02 08:29:24 -0500

    updated footer and scroll feature

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ document.querySelector('.fa-bars').addEventListener('click', () => {
     const nav =  document.querySelector('.nav_links');
     nav.classList.toggle('hide');
 })
+
+const backToTop = document.getElementById('back-to-top')
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 700) {
+        backToTop.classList.remove('hidden')
+    } else {
+        backToTop.classList.add('hidden')
+    }
+})
+
+backToTop.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0, 
+        behavior: 'smooth'
+    })
+})
