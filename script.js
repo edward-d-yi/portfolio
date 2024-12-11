2239f4471ef627660734f2ade8340ff9955f2ce5
Edward-D-Yi <163349148+Edward-D-Yi@users.noreply.github.com>
2024-12-11 18:48:12 -0500

    fixed resume tab and footer

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
+window.onload = () => {
+    const today = new Date()
+    const year = today.getFullYear()
+    document.querySelector('#year').innerHTML = year
+}
+
 document.querySelector('.fa-bars').addEventListener('click', () => {
-    const nav =  document.querySelector('.nav_links');
+    const nav = document.querySelector('.nav_links');
     nav.classList.toggle('hide');
 })
 
-const backToTop = document.getElementById('back-to-top')
+if (location.href.includes('index')) {
+    const backToTop = document.getElementById('back-to-top')
 
-window.addEventListener('scroll', () => {
-    if(window.scrollY > 1400) {
-        backToTop.classList.remove('hidden')
-    } else {
-        backToTop.classList.add('hidden')
-    }
-})
-
-backToTop.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0, 
-        behavior: 'smooth'
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 1400) {
+            backToTop.classList.remove('hidden')
+        } else {
+            backToTop.classList.add('hidden')
+        }
     })
-})
+
+    backToTop.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    })
+}
